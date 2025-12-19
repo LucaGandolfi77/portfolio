e533fc4671bb823221f691ac982c4abb61ee0b97
LucaGandolfi77 <90201251+LucaGandolfi77@users.noreply.github.com>
2025-12-19 15:50:25 +0000

    fix: fixed rpg game

--- rpg_game/src/game/PhaserGame.ts
+++ rpg_game/src/game/PhaserGame.ts
@@ -14,7 +14,7 @@ export const PhaserGame = (containerId: string) => {
     physics: {
       default: 'arcade',
       arcade: {
-        gravity: { y: 0 },
+        gravity: { x: 0, y: 0 },
         debug: false
       }
     },
